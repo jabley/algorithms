060fc74356b9635122bc0adcce4518cab8d80168
James Abley <james.abley@gmail.com>
2017-01-09 08:53:51 +0000

    Copy FixedCapacityStackOfStrings from book.
    Going to start implementing solutions to the exercises.

---
+++ src/main/java/chapter/first/FixedCapacityStackOfStrings.java
@@ -0,0 +1,26 @@
+package chapter.first;
+
+public class FixedCapacityStackOfStrings {
+    private final String a[];
+    private int N;
+
+    public FixedCapacityStackOfStrings(int capacity) {
+      this.a = new String[capacity];
+    }
+
+    public boolean isEmpty() {
+      return N == 0;
+    }
+
+    public int size() {
+      return N;
+    }
+
+    public void push(String item) {
+      a[N++] = item;
+    }
+
+    public String pop() {
+      return a[--N];
+    }
+}
